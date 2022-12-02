bad876450c3336d15db37610b7a2627eda998ccf
hamed <hamedmohamed@yahoo.com>
2022-12-02 14:38:09 +0200

    add helloGit method to helloIti class

--- HelloIti.java
+++ HelloIti.java
@@ -1,9 +1,14 @@
 class HelloIti{
 	public static void main(String[] args) {
 		printHelloIti();
+		printHelloGit();
 	}
 
 	public void printHelloIti(){
 		System.out.println("hello iti");
+	}
+
+	public void printHelloGit(){
+		System.out.println("hello Git");
 	}
 }
